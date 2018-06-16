6d8c9d0924d66a360cf54092b18040aa3dfe75ef
Harjeet <singh.fulda@gmail.com>
2018-06-16 22:07:05 +0200

    15. Async Await
    Promises can be dissolved by using async and await in node.js -v>7.0

--- server.js
+++ server.js
@@ -29,34 +29,39 @@ app.get('/messages', (req,res) => {
     
 })
 
-app.post('/messages', (req,res) => {
+app.post('/messages', async (req, res) => {
     var message = new Message(req.body)
-    message.save()
-    .then(() => {
-        console.log('saved')
-        return Message.findOne({message: 'badword'})
-    })
-    .then(censored => {
-        if(censored)  {
-             console.log('censored words found', censored)
-             return Message.remove({_id: censored.id})
-         }
-        io.emit('message', req.body)
-        res.sendStatus(200) 
+    var savedMessage = await message.save()
 
-    })
-    .catch((err) => {
-        res.sendStatus(500)
-        return console.error(err)
-    })
+    console.log('saved')
+    var censored = await Message.findOne({message: 'badword'})
+ 
+    if(censored) 
+        await Message.remove({_id: censored.id})
+    else
+       io.emit('message', req.body)
+
+    res.sendStatus(200) 
+
+  
+    // .catch((err) => {
+    //     res.sendStatus(500)
+    //     return console.error(err)
+    // })
    
 })
+
+
+
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
+
+
 mongoose.connect(dburl, (err)=>{
     console.log('db connection',err)
 })
+
 var server = http.listen(3000, () =>{
     console.log('server is listening on port ' + server.address().port)
 })
