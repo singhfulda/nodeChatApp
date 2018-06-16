9eac095c414ef72414399147d8999ad85ada9e91
Harjeet <singh.fulda@gmail.com>
2018-06-16 13:09:26 +0200

    8. creating message api
    create backend to send messages  to frontend and calling in frontend using $.get()

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name:'Singh', message:'Hi'},
+    {name:'Schmidt', message:'Hallo'}
+]
+
+
+app.get('/messages', (req,res) => {
+    res.send(messages)
+})
+
+
 var server = app.listen(3000, () =>{
     console.log('server is listening on port ' + server.address().port)
 })
