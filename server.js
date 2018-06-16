8229071ecddf32938bd37aa941ad313e182e9873
Harjeet <singh.fulda@gmail.com>
2018-06-16 13:33:42 +0200

    9. creating post api
    post api created
    used bodyparser to tell json is coming
    used bodyParser to urlencoded data to be read
    $.post in front end to put data to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name:'Singh', message:'Hi'},
@@ -11,6 +14,12 @@ var messages = [
 
 app.get('/messages', (req,res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 
